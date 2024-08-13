448aae0c9e0966e59d8e502a3eefb90c704620c9
“Chin-TT” <20010757@st.phenikaa-uni.edu.vn>
2024-08-14 02:30:11 +0700

    fix bug and css mobile

--- src/app/image-tools/ai-generator/page.tsx
+++ src/app/image-tools/ai-generator/page.tsx
@@ -1,18 +1,7 @@
-import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
-
 export default function () {
   return (
     <div className="w-full h-full text-center">
       <h1>AI generator </h1>
-      <div className="flex border  border-red-500 ">
-        <Textarea
-          placeholder="Type your message here."
-          className="w-[500px] h-[100px]"
-        />
-        <Button className="ml-5">Generate</Button>
-      </div>
-      <div className="w-[900px] h-[500px] border border-blue-400 mx-auto"></div>
     </div>
   );
 }
